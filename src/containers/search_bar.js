65dbbd5a91b421bd6006dbd8110b5c0022a32822
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-04-17 21:25:40 -0500

    added reducer and switch case

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state={ term: '' };
     // Sets 'this' to the correct context
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -19,6 +20,8 @@ export default class SearchBar extends Component {
   onFormSubmit(event) {
     event.preventDefault();
 
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: ''});
   }
 
   render() {
@@ -40,3 +43,5 @@ export default class SearchBar extends Component {
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchWeather }, dispatch);
 }
+
+export default connect(null, mapDispatchToProps)(SearchBar);
