bce3749575eba737c2a319a295c873d920f54888
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-04-28 08:30:57 -0500

    add google maps to results

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,6 +9,8 @@ class WeatherList extends Component {
     const kelvinTemps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    // ES6 destructuring grab longitude & latitude from cityData.city.coord object
+    const { lon, lat } = cityData.city.coord;
 
     function convertToFahrenheit(temp) {
       return Math.floor(9 / 5 * (temp -273.15) + 32);
@@ -18,7 +21,7 @@ class WeatherList extends Component {
     console.log(temps);
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap  lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange" units="F" /></td>
         <td><Chart data={pressures} color="green" units="hPa" /></td>
         <td><Chart data={humidities} color="black" units="%" /></td>
